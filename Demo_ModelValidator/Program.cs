d2bf2876c603caf73f23c8aec321404a93463dc5
freeman6 <freeman6@gmail.com>
2019-04-12 15:09:50 +0800

    final

--- Demo_ModelValidator/Program.cs
+++ Demo_ModelValidator/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using CSF.ModelValidator;
 
 namespace Demo_ModelValidator
 {
     class Program
     {
+        static ModelState modelstatus = new ModelState();
         static void Main(string[] args)
         {
+
+            var reco = new Person { ID = "F123456789", Age = -9, CName = "康文龍", Mobile = "0922626523", EMail = "reco@mail.csf.org.tw" };
+            if (modelstatus.IsVaild<Person>(reco) == true)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                foreach (var item in modelstatus.MemberNames)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            //modelstatus.AddModelError("test0000");
+            Console.WriteLine(modelstatus["Age"]);
+            Console.WriteLine(modelstatus.ModelErrorMessage);
+            Console.ReadLine();
         }
+    }
+
+    public class Person
+    {
+        public string ID { get; set; }
+        [Chinese]
+        public string CName { get; set; }
+        [Age]
+        public int Age { get; set; }
+        [Mobile]
+        public string Mobile { get; set; }
+        [Email]
+        public string EMail { get; set; }
     }
 }
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Demo 如何使用 ModelValidator Component
+
+### TDC 標準 .NET 處理 Model Vallid 元件
+***
+### Installation Instructions
+#### Nuget Client:
+<code>nuget install CSF.Attribute.ModelValidator -Version 1.1.0 -Source http://nuget.csf.org.tw/nuget/internal/</code>
+
+#### Package Manager Console:
+<code>Install-Package CSF.Attribute.ModelValidator -Version 1.1.0 -Source http://nuget.csf.org.tw/nuget/internal/</code>
